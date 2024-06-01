0082fce727df7deae15119e7a4ba2ae16a1f5c01
GreatKuriGohan <caverly228@gmail.com>
2024-06-01 21:53:23 +0500

    admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from products.models import UsersInfo, Computers, Reservations
 # Register your models here.
+admin.site.register(UsersInfo)
+admin.site.register(Computers)
+admin.site.register(Reservations)
