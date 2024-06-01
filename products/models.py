4e615b6cb0d18441f2a05a1cb9f764e279544bc4
GreatKuriGohan <caverly228@gmail.com>
2024-06-01 22:12:43 +0500

    admin2

--- products/models.py
+++ products/models.py
@@ -6,10 +6,17 @@ class UsersInfo(models.Model):
     email = models.EmailField(null=True)
     user_name = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
+    def __str__(self):
+        return self.user_name
 
 
 class Computers(models.Model):
+    computer_name = models.CharField(max_length=128)
     availability_status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.computer_name
+
 
 class Reservations(models.Model):
     user_id = models.ForeignKey(UsersInfo, on_delete=models.CASCADE)
@@ -17,3 +24,5 @@ class Reservations(models.Model):
     reservation_date = models.DateField()
     reservation_time = models.TimeField()
 
+    def __str__(self):
+        return f'Пользователь: {self.user_id} | Компьютер #: {self.computer_id}'
