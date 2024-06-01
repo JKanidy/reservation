a71159294b20a5800db8c239eb2fcb4cafbe4a47
JKanidy <jorakanidi@gmail.com>
2024-06-01 17:59:13 +0500

    возня с css файлами

--- products/views.py
+++ products/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'test1',
+               'username': 'jora',
+               }
+    return render(request, 'products/index.html', context)
 
 def products(request):
     return render(request, 'products/products.html')
+
+def cabinet(request):
+    return render(request, 'products/cabinet.html')
