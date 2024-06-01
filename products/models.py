970986dbea2f4c2f3dd91439b94380548a24fef9
JKanidy <jorakanidi@gmail.com>
2024-06-01 19:59:16 +0500

    вторая миграция

--- products/models.py
+++ products/models.py
@@ -3,16 +3,12 @@ from django.db import models
 # Create your models here.
 
 class UsersInfo(models.Model):
-    name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=75)
-    number = models.TextField(null=True)
     email = models.EmailField(null=True)
     user_name = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
 
 
 class Computers(models.Model):
-    computer_name = models.CharField(max_length=50)
     availability_status = models.BooleanField(default=True)
 
 class Reservations(models.Model):
